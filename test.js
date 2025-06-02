c0048c2182a16f6ed58aca10319ff574867fcff8
anjulrod <anjulrod123@gmail.com>
2025-06-02 16:56:34 +0530

    first file

---
+++ test.js
@@ -0,0 +1,33 @@
+let marks= 95;
+let grade;
+if
+(marks >=80 && marks <=100){
+    grade="Distinction"
+}else if
+(marks >=60 && marks <=79){
+    grade="First Class"
+}else if
+    (marks >=50 && marks <=59){
+    grade="Second Class"
+}
+else if
+    (marks >=35 && marks <=49){
+    grade="Pass"
+}
+else{
+    (marks >=0 && marks <=34)
+grade="FAIL"
+}
+console.log(grade)
+
+
+let age = 15;
+if
+(age >=18)
+{
+console.log ("you are eligible to vote");
+}
+else
+{
+console.log(`you are not eligible to vote wait for ${18 - age}`);  
+}
