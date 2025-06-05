fd5631752bc8b923f8d652659048e58b77e09c67
anjulrod <anjulrod123@gmail.com>
2025-06-05 07:51:35 +0530

    for loop

---
+++ javascript/loops.js
@@ -0,0 +1,87 @@
+
+// ternary operators
+
+// let num= 15
+// result= num>10 ? "High" : "low"
+// console.log(result)
+
+// let age= 19
+// result = age>18 ? "eligible" : "not eligible"
+// console.log(result)
+//   let age =15
+    
+// let result = age>18 ? "adult" : "not adult";
+//     console.log(result)
+
+    // let age= 15
+    // result= age>10 ? "true": "false"
+    // console.log(result) 
+//  let num =51
+//  let score = num >50 ? "pass": "fail"
+//  console.log(score)
+
+// for loop
+
+
+// for (let i = 1; i <= 3; i++) {
+//   console.log(i);
+// }
+
+// for(let i=10; i>0; i--){
+//     console.log(i)
+// }
+// for(i =1; i<=10; i++){
+//     console.log(i)
+// }
+// for(num=2; num<=10; num+=2){
+// console.log(num)
+// }
+//   for(i=1; i<10; i++){
+    // console.log("3 x" + i + "=" + (i*3))
+//   }
+
+//for of loop//
+
+// let fruits=["apple", "mango", "banana"]
+// for(let fruit of fruits){
+//     console.log(fruit)
+// }
+
+// let word = "India";
+// for (let char of word) {
+//   console.log(char);
+// }
+
+//for in//
+
+// let count= "India"
+// for(let count1 in count){
+//     console.log(count1)
+
+// }
+let x=["ind", "Nz", "pak", "Sl", "Aus"]
+for(let y in x){
+    console.log(x[y])
+}
+
+// while loop//
+
+// let a=0
+// while(a<=5){
+//     console.log(a)
+//     a++
+// }
+// let num=2
+// while(num<=10){
+//     console.log(num)
+//     num+=2
+// }
+
+let table=1
+while(table<=10){
+    console.log("4 x "+ table + "=" + (4*table))
+    table++
+}
+
+
+
