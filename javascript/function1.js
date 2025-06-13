87403151e386223b124b9b20e2e2fc4a72d989a2
anjulrod <anjulrod123@gmail.com>
2025-06-13 07:56:03 +0530

    practics on destructuring

---
+++ javascript/function1.js
@@ -0,0 +1,116 @@
+// let fname="Anjul     "
+// let lname="Rod"
+// let fullname= fname.trim() +" "+ lname
+// fullname=fullname.toUpperCase()
+// console.log(fullname);
+
+// fname="Shlok"
+// lname="Chaudhary"
+// fullname= fname +" "+ lname
+// fullname=fullname.toUpperCase()
+// console.log(fullname);
+
+//function
+
+// let fname
+// let lname 
+// let full_name   
+// function person_name(fname, lname) {
+// full_name= fname.toUpperCase() + " " + lname.toUpperCase()
+// console.log("Full name of person is " + full_name);
+// }
+// person_name("Anjul", "Rod")
+// person_name("shlok", "chaudhary")
+// person_name("ankit", "chaudhary")
+
+
+// function per(name, age, city) {
+// console.log("Hi.. My name is " + name +". I am " + age + "years old" +". I live in " + city);
+// }
+// per("Anjul", 36, "Delhi")
+// per("Shlok", 7, "Delhi")
+
+// argument
+
+// function arg() {
+//     for(let arg1 in arguments){
+// console.log("Arguments#" + parseInt(arg1) + " is " + arguments[arg1]);}   
+// }
+// arg(124, 123535, 45325, 45654)
+
+
+
+
+//self invoking
+// (function(num){
+//     let num1= num * num
+//     console.log(num1);
+// }
+// )(50)
+
+// expression function
+
+// let num=function(num1){
+// let sqr= num1 * num1
+// console.log(sqr);
+// }
+// num(5)
+// num(25)
+
+
+// function greet() {
+// console.log("hello world");
+// }
+// greet()
+
+// function add(a, b) {
+// return a + b  
+// }
+// let result=add(4, 5)
+// console.log(result);
+
+// function sqr(num, num1) {
+//     return num * num1   
+// }
+// let result= sqr(10,5)
+// console.log(result);
+
+// function isEven(num) {
+// return num % 2===0   
+// }
+// console.log(isEven(2));
+// console.log(isEven(3));
+
+// function even(num) {
+// if(num%2===0){
+// console.log(num + " is even")
+// }
+// else{
+//     console.log(num + " is odd");
+// }
+// }
+
+// even(10)
+// even(3)
+
+
+// function table(n) {
+//     for(let i=1; i<=10; i++)
+// console.log(n + "x" + i + "=" + i * n);
+// }
+// table(2)
+
+// function fac(n) {
+//     let result=1
+//     for(let i=1; i<=n; i++){
+// result *=i        
+//     }
+//         return result
+// }
+// console.log(fac(5));
+
+
+
+
+ 
+ 
